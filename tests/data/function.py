4cb8f506bc0e0ed18d28d603b532123df56c95f0
Matthieu Devlin <matt@zumper.com>
2020-08-30 08:14:11 -0700

    Back to 2 space indents - tests

--- tests/data/function.py
+++ tests/data/function.py
@@ -214,12 +214,12 @@ def func_no_args():
 
 
 def function_signature_stress_test(
-    number: int,
-    no_annotation=None,
-    text: str = 'default',
-    *,
-    debug: bool = False,
-    **kwargs
+  number: int,
+  no_annotation=None,
+  text: str = 'default',
+  *,
+  debug: bool = False,
+  **kwargs
 ) -> str:
   return text[number:-1]
 
@@ -230,15 +230,15 @@ def spaces(a=1, b=(), c=[], d={}, e=True, f=-1, g=1 if False else 2, h='', i=r''
 
 
 def spaces_types(
-    a: int = 1,
-    b: tuple = (),
-    c: list = [],
-    d: dict = {},
-    e: bool = True,
-    f: int = -1,
-    g: int = 1 if False else 2,
-    h: str = '',
-    i: str = r'',
+  a: int = 1,
+  b: tuple = (),
+  c: list = [],
+  d: dict = {},
+  e: bool = True,
+  f: int = -1,
+  g: int = 1 if False else 2,
+  h: str = '',
+  i: str = r'',
 ):
   ...
 
@@ -249,37 +249,37 @@ def spaces2(result=_core.Value(None)):
 
 def example(session):
   result = (
-      session.query(models.Customer.id)
-      .filter(
-          models.Customer.account_id == account_id,
-          models.Customer.email == email_address,
-      )
-      .order_by(models.Customer.id.asc())
-      .all()
+    session.query(models.Customer.id)
+    .filter(
+      models.Customer.account_id == account_id,
+      models.Customer.email == email_address,
+    )
+    .order_by(models.Customer.id.asc())
+    .all()
   )
 
 
 def long_lines():
   if True:
     typedargslist.extend(
-        gen_annotated_params(
-            ast_args.kwonlyargs,
-            ast_args.kw_defaults,
-            parameters,
-            implicit_default=True,
-        )
+      gen_annotated_params(
+        ast_args.kwonlyargs,
+        ast_args.kw_defaults,
+        parameters,
+        implicit_default=True,
+      )
     )
     typedargslist.extend(
-        gen_annotated_params(
-            ast_args.kwonlyargs,
-            ast_args.kw_defaults,
-            parameters,
-            implicit_default=True,
-            # trailing standalone comment
-        )
+      gen_annotated_params(
+        ast_args.kwonlyargs,
+        ast_args.kw_defaults,
+        parameters,
+        implicit_default=True,
+        # trailing standalone comment
+      )
     )
   _type_comment_re = re.compile(
-      r"""
+    r"""
         ^
         [\t ]*
         \#[ ]type:[ ]*
@@ -298,29 +298,29 @@ def long_lines():
         )
         $
         """,
-      re.MULTILINE | re.VERBOSE,
+    re.MULTILINE | re.VERBOSE,
   )
 
 
 def trailing_comma():
   mapping = {
-      A: 0.25 * (10.0 / 12),
-      B: 0.1 * (10.0 / 12),
-      C: 0.1 * (10.0 / 12),
-      D: 0.1 * (10.0 / 12),
+    A: 0.25 * (10.0 / 12),
+    B: 0.1 * (10.0 / 12),
+    C: 0.1 * (10.0 / 12),
+    D: 0.1 * (10.0 / 12),
   }
 
 
 def f(
-    a,
-    **kwargs,
+  a,
+  **kwargs,
 ) -> A:
   return (
-      yield from A(
-          very_long_argument_name1=very_long_value_for_the_argument,
-          very_long_argument_name2=very_long_value_for_the_argument,
-          **kwargs,
-      )
+    yield from A(
+      very_long_argument_name1=very_long_value_for_the_argument,
+      very_long_argument_name2=very_long_value_for_the_argument,
+      **kwargs,
+    )
   )
 
 
@@ -329,15 +329,15 @@ def __await__():
 
 
 def some_long_function_name_with_args_that_fit_on_one_line(
-    argument_number1,
-    argument_number2,
-    argument_number3,
+  argument_number1,
+  argument_number2,
+  argument_number3,
 ):
   return None
 
 
 NAMESPACED_FIELDSETS = {
-    'agents': [typed_fields.LongName()],
+  'agents': [typed_fields.LongName()],
 }
 
 NAMESPACED_FIELDSETS = {'agents': [typed_fields.LongName()]}
@@ -346,58 +346,58 @@ NAMESPACED_FIELDSETS = {'agents': [typed_fields.LongName()]}
 @links.links('Zapp Activity')
 def _zapp_activity(self, agent):
   return links.admin_link(
-      agent,
-      'creditactivity',
-      text=str(agent.user_id),
-      params={'user_id': agent.user_id},
+    agent,
+    'creditactivity',
+    text=str(agent.user_id),
+    params={'user_id': agent.user_id},
   )
 
 
 def setUp(self):
   super(MMExcludeMonthlyRemoveTest, self).setUp()
   self.ensure_exists(
-      objects.Account,
-      account_id=1,
-      defaults={'name': 'Account #1', 'salesforce_id': '#1'},
+    objects.Account,
+    account_id=1,
+    defaults={'name': 'Account #1', 'salesforce_id': '#1'},
   )
 
 
 building_high_vol = self.add_building(
-    status=enums.ListingStatus.ACTIVE,
-    deal_id=deal.pk,
-    masked_fields=zjson.dumps({'features': [enums.PropertyFeatures.MM_EXCLUDE]}),
-    features=enums.PropertyFeatures.encode_bitmask(
-        [enums.PropertyFeatures.HIGH_VOLUME, enums.PropertyFeatures.MM_EXCLUDE]
-    ),
+  status=enums.ListingStatus.ACTIVE,
+  deal_id=deal.pk,
+  masked_fields=zjson.dumps({'features': [enums.PropertyFeatures.MM_EXCLUDE]}),
+  features=enums.PropertyFeatures.encode_bitmask(
+    [enums.PropertyFeatures.HIGH_VOLUME, enums.PropertyFeatures.MM_EXCLUDE]
+  ),
 )
 
 something = ['abc']
 
 something_else = [
-    'abc',
+  'abc',
 ]
 
 expected_ptypes = itertools.chain.from_iterable(
-    x[1] for x in api.PROPERTY_CATEGORIES.iteritems() if x[0] in site_pcats
+  x[1] for x in api.PROPERTY_CATEGORIES.iteritems() if x[0] in site_pcats
 )
 
 FORMFIELD_OVERRIDES = {
-    model_utils.ArrayField: {
-        'widget': admin_widgets.AdminTextInputWidget(
-            attrs={
-                'cols': 240,
-                'class': 'vLargeTextField',
-            }
-        )
-    },
+  model_utils.ArrayField: {
+    'widget': admin_widgets.AdminTextInputWidget(
+      attrs={
+        'cols': 240,
+        'class': 'vLargeTextField',
+      }
+    )
+  },
 }
 
 something = (1,)
 
 tuple_with_stuff = (
-    1,
-    2,
-    3,
+  1,
+  2,
+  3,
 )
 
 some = function('hello', 'world')
